d2c923a899a28967add42b73f4733a455439022e
Nicole W <Nicole.Wilkins2018@gmail.com>
2018-11-17 12:25:12 -0500

    trying for heroku

--- server.js
+++ server.js
@@ -1,2 +1,42 @@
 var express = require("express")
 var path = require("path")
+
+var express = require("express");
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "wishes_db"
+});
+
+
+
+
+
+
+
+
+  // Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
