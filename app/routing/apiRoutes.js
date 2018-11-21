cfab57d4e2a648970128ee52c459f2dc901dfa51
Nicole W <Nicole.Wilkins2018@gmail.com>
2018-11-20 21:09:18 -0500

    adding up numbers

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,20 @@ module.exports = function(app) {
         res.json(dogData);
     });
 
-    app.post("/api/friends", function(req, res) {
-       console.log(req.body); 
+    app.post("/api/friends", sum, function(req, res) {
+       //puggle is 17, so matches would be equal or less then 17
+       if(sum <= 17) {
+           display dogArray[2]
+       }
+       //mastiff is 22, so matches would be equal or grater then 22
+       else if(sum >= 22) {
+           display dogArray[3]
+       }
+       //french bulldog is 19, so matches are 18 through 21
+       else {
+           display dogArray[1]
+       }
+       
+        console.log(req.body); 
     })
 }
